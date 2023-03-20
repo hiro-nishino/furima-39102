f3a2700a75980b6884367c3abc593ceb6f876e97
hiro-nishino <126562444+hiro-nishino@users.noreply.github.com>
2023-03-20 18:51:39 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-
   with_options presence: true,
    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字で入力してください' } do
     validates :first_name
@@ -14,7 +11,7 @@ class User < ApplicationRecord
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, presence: true, length: { minimum: 6 } 
+  validates_format_of :password, with: PASSWORD_REGEX
 
 
   with_options presence: true,
