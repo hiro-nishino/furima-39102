0aa38b340986ebd6853aaba55cd5d5f3331c580c
hiro-nishino <126562444+hiro-nishino@users.noreply.github.com>
2023-03-21 10:33:37 +0900

    Active Storage インストール

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,23 @@ class ItemsController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+    
+    private
+    def item_params
+      params.require(:item).permit(:name, :detail, :item_category_id, :item_condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price, {images: []}).merge(user_id: current_user.id)
+    end
+  end
 end
 
