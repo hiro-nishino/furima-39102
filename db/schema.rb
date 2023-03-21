7aa89200d9f57f06765bb3d1f4a5c09327449856
hiro-nishino <126562444+hiro-nishino@users.noreply.github.com>
2023-03-21 09:04:01 +0900

    items DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_19_102637) do
+ActiveRecord::Schema.define(version: 2023_03_20_232310) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "detail", null: false
+    t.integer "item_category_id", null: false
+    t.integer "item_condition_id", null: false
+    t.integer "shipping_charge_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "days_to_ship_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "nickname", null: false
@@ -31,4 +44,5 @@ ActiveRecord::Schema.define(version: 2023_03_19_102637) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
