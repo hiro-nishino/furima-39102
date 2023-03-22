338ff53a5473068dd76726ea2602ace810867379
hiro-nishino <126562444+hiro-nishino@users.noreply.github.com>
2023-03-22 19:42:16 +0900

    編集設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit, :update,]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -17,7 +19,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   def show
+  end
+
+  def set_item
     @item = Item.find(params[:id])
   end
 
@@ -26,4 +42,10 @@ class ItemsController < ApplicationController
     def item_params
       params.require(:item).permit(:name, :detail, :item_category_id, :item_condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
     end
+
+  def move_to_index 
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
  end  
