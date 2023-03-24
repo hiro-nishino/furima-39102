b34bf7b3b7daa651a105d369cea8eb9b386d998d
hiro-nishino <126562444+hiro-nishino@users.noreply.github.com>
2023-03-24 16:54:55 +0900

    環境変数設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,37 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_find, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
   
   def create
-    binding.pry
-    @item = Item.find(params[:item_id])
-    @order_address = OrderAddress.new(order_address_params)
-    
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
-
   private
-  def order_address_params
-    params.require(:item).permit(:post_code, :prefecture_id, :city, :addresses, :building_name, :phone_number,).merge(user_id: current_user.id, item_id:@item.id)
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :addresses, :building_name, :phone_number,).merge(user_id: current_user.id, item_id:@item.id, token: params[:token])
+  end
+
+  def set_find
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item[:price],
+        card: order_params[:token],
+        currency: 'jpy'
+      )
   end
 end
