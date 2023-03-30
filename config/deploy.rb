05747346aebf758153574a3802ae12c6b53ce6ae
hiro-nishino <126562444+hiro-nishino@users.noreply.github.com>
2023-03-30 16:57:05 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.17.2"
 
 set :application, "furima_39102"
-set :repo_url, "git@example.com:hiro-nishino/furima-39102"
+set :repo_url, "git@example.com:hiro-nishino/furima_39102"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
